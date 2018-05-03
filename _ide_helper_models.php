f25ff570fd467b0a16d6727bd161593c330a5706
Oleksii Yaryi <oleksii.yaryi@gmail.com>
2018-05-03 18:53:22 +0300

    updated to analog Skeleton

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -8,35 +8,3 @@
  */
 
 
-namespace Modules\Users\Models{
-/**
- * Modules\Users\Models\User
- *
- * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
- * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Users\Models\Permission[] $permissions
- * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Users\Models\Role[] $roles
- * @property-write mixed $password
- * @method static \Illuminate\Database\Eloquent\Builder|\Modules\Users\Models\User wherePermissionIs($permission = '')
- * @method static \Illuminate\Database\Eloquent\Builder|\Modules\Users\Models\User whereRoleIs($role = '')
- */
-	class User extends \Eloquent {}
-}
-
-namespace Modules\Users\Models{
-/**
- * Modules\Users\Models\Permission
- *
- * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Users\Models\Role[] $roles
- */
-	class Permission extends \Eloquent {}
-}
-
-namespace Modules\Users\Models{
-/**
- * Modules\Users\Models\Role
- *
- * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Users\Models\Permission[] $permissions
- */
-	class Role extends \Eloquent {}
-}
-
--- modules/Users/Http/Controllers/Auth/ResetPasswordController.php
+++ modules/Users/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,11 +3,14 @@
 namespace Modules\Users\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 use Illuminate\Http\Request;
+use Psy\Util\Str;
 
 class ResetPasswordController extends Controller
 {
+
     /*
     |--------------------------------------------------------------------------
     | Password Reset Controller
@@ -29,12 +32,27 @@ class ResetPasswordController extends Controller
     protected $redirectTo = '/home';
 
     /**
+     * @param $user
+     * @param $password
+     */
+    protected function resetPassword($user, $password)
+    {
+        $user->password = $password;
+        $user->setRememberToken(Str::random(60));
+        $user->save();
+
+        event(new PasswordReset($user));
+
+        $this->guard()->login($user);
+    }
+
+    /**
      * Display the password reset view for the given token.
      *
      * If no token is present, display the link request form.
      *
      * @param \Illuminate\Http\Request $request
-     * @param string|null              $token
+     * @param string|null $token
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
